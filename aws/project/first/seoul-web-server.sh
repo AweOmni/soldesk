22d0f9fc3a72424305ac2e1171e7e86d41335fed
AweOmni <157345706+AweOmni@users.noreply.github.com>
2024-04-12 12:19:41 +0900

    Update seoul-web-server.sh

--- aws/project/first/seoul-web-server.sh
+++ aws/project/first/seoul-web-server.sh
@@ -4,5 +4,5 @@ systemctl start httpd
 systemctl enable httpd
 cat <<EOF > /var/www/html/index.html
 <h1>서울 웹 서버에 오신 것을 환영합니다.</h1>
-<img sec='https://img.omnistudy.xyz/seoul.jpg' width='800'>
+<img src='https://img.omnistudy.xyz/seoul.jpg' width='800'>
 EOF
